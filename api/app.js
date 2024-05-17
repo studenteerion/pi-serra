b179526ae373fc15abc6bd35ecb438fe6f1a7ade
Locatelli Alex <locatelli.alexgiannino.studente@itispaleocapa.it>
2024-05-17 14:20:39 +0200

    Moved newapp.js code to app.js

--- api/app.js
+++ api/app.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const express = require("express")
+const bodyParser = require("body-parser")
+  
 const app = express();
 const { createServer } = require("node:http");
 const cors = require('cors');
-
-const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const db = require('./functions/db');
-const sensors = require('./functions/sensors');
-const socket = require('./functions/socket')
+const SensorValues = require('./functions/db_dyn_schema')
+const swagger = require('./swagger')
+const getJson = require('./functions/getjson')
+const sensorList = require('./config_files/sensors_list.json')
 
 app.use(cors());
 
@@ -20,21 +22,12 @@ app.use(
 );
 
 const server = createServer(app); // avvio server express
-socket.createSocket(server)
 app.use(express.static("public"));
+swagger(app)
 
-socket.listenForMainConnection()
-socket.listenForPanel()
-
-//ultima lettura nel db
-app.get("/api/lastdata", async (_, res) => {
-  res.json(await db.getData(true));
-});
-
-//tutte le letture del db
-app.get("/api/alldata", async (_, res) => {
-  res.json(await db.getData(false));
-});
+//impostazione delle routes
+app.use('/sensors', require('./routes/sensors'));
+app.use('/controls', require('./routes/controls'));
 
 (async () => {
   await db.connect();
@@ -44,8 +37,45 @@ app.get("/api/alldata", async (_, res) => {
     console.log("Server started at port 8080");
   });
 
+  //polling di tutti sensori da eseguire periodicamente
+  setInterval(async () => {
+    try {
+      const dataPromises = sensorList.map(device => processDevice(device));
+      const allData = await Promise.all(dataPromises);
+      
+      // Assuming `Data` is your Mongoose model
+      const data = new SensorValues({ valueList: [].concat(...allData.map(data => data.valueList)) });
+      console.log(data);
+      await data.save();
+
+      console.log(`All data saved successfully.`);
+
+  } catch (error) {
+      console.error(`Error processing devices: ${error}`);
+  }
+  }, process.env.UpdateFrequencySeconds * 1000)
+
 });
 
-//richiesta dati ai sensori
-//invocazione della funzione la prima volta in modo che i dati non siano vuoti appena di accede all'applicazione
-sensors.getSensorData();
+async function processDevice(device) {
+  try {
+      const taskValues = await getJson.sensorJSON(device.url);
+      const transformedData = transformData(taskValues);
+      
+      console.log(`Data for device ${device.id} fetched successfully.`);
+      return transformedData;
+
+  } catch (error) {
+      console.error(`Error processing device ${device.id}: ${error}`);
+  }
+}
+
+function transformData(taskValues) {
+  return {
+      date: new Date(),
+      valueList: taskValues.map(taskValue => ({
+          description: taskValue.Name,
+          value: taskValue.Value.toString()
+      }))
+  };
+}
